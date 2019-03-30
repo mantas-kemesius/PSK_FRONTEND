d3eb7649ab0619249c48717f83cbabd2bbf2a41b
Mantas Kemėšius <mantaskemesius@Mantass-Air.lan>
2019-03-30 15:24:48 +0200

    setup react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import * as serviceWorker from "./serviceWorker";
 import "./styles";
+import { Provider } from "react-redux";
+import configureStore from "./store/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
