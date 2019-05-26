3797ada13032c01a9a5b7aeee4f72010ecf6e906
Mantas Kemėšius <mantaskemesius@Mantass-MacBook-Air.local>
2019-05-26 20:20:55 +0300

    add hover action

--- src/components/trips/table/rows/Component.js
+++ src/components/trips/table/rows/Component.js
@@ -1,20 +1,55 @@
 import React from "react";
 import Columns from "./../columns/Container";
 
-const Rows = ({ ids, ready }) => {
-  if (!ready) {
-    return null;
+const active = { style: { backgroundColor: "#f1f1f1" }, className: "cp fw500" };
+const notActive = { style: {}, className: "" };
+
+class Rows extends React.Component {
+  state = {
+    hoverArr: [],
+    set: false
+  };
+
+  componentDidUpdate() {
+    if (!this.state.set && this.props.ids.length > 0) {
+      const hoverArr = [];
+      this.props.ids.forEach((i, key) => hoverArr.push(false));
+      this.setState({
+        hoverArr,
+        set: true
+      });
+    }
   }
 
-  return (
-    <>
-      {ids.map(item => (
-        <tr key={`tb-row-${item}`}>
-          <Columns id={item} />
-        </tr>
-      ))}
-    </>
-  );
-};
+  toggleHover = (hovered, key) => {
+    const { hoverArr } = this.state;
+    hoverArr[key] = hovered;
+    this.setState({
+      hoverArr
+    });
+  };
+
+  render() {
+    if (!this.props.ready) {
+      return null;
+    }
+    const { hoverArr } = this.state;
+    return (
+      <>
+        {this.props.ids.map((item, key) => (
+          <tr
+            className={hoverArr[key] ? active.className : notActive.className}
+            key={`tb-row-${item}`}
+            style={hoverArr[key] ? active.style : notActive.style}
+            onMouseOver={() => this.toggleHover(true, key)}
+            onMouseOut={() => this.toggleHover(false, key)}
+          >
+            <Columns id={item} />
+          </tr>
+        ))}
+      </>
+    );
+  }
+}
 
 export default Rows;
