08db6b9bcd9ceb2abd223353c6d66ad4619b7a50
Mantas Kemėšius <mantaskemesius@Mantass-MacBook-Air.local>
2019-05-26 18:45:09 +0300

    update stats

--- src/components/availabilityDiagram/Component.js
+++ src/components/availabilityDiagram/Component.js
@@ -50,40 +50,44 @@ class DynamicColumnChart extends Component {
             />
           </div>
           <div className="df jc-s-evenly">
-            <div className="w30p">
-              <select
-                className="w100p h50 p10 fz18 b-s1-grey"
-                onChange={this.handleFormatChange}
-              >
-                <option key="1" value="jpg">
-                  JPG
-                </option>
-                <option key="2" value="png">
-                  PNG
-                </option>
-                <option key="3" value="pdf">
-                  PDF
-                </option>
-              </select>
-            </div>
-            <div className="w30p">
-              <button
-                className="w100p h50 p10 fz18 bg-default cw fwb"
-                onClick={() => {
-                  if (this.state.format === "pdf") {
-                    const dataURL = document
-                      .getElementsByClassName("canvasjs-chart-canvas")[0]
-                      .toDataURL();
-                    var pdf = new jsPDF();
-                    pdf.addImage(dataURL, "JPEG", 20, 20, 170, 80);
-                    pdf.save("download.pdf");
-                  } else {
-                    this.chart.exportChart({ format: this.state.format });
-                  }
-                }}
-              >
-                Atsisiūsti
-              </button>
+            <div className="w60p" />
+            <div className="w40p df">
+              <div className="pr20 h100p fz20 fwb pt10">Formatas: </div>
+              <div className="w40p pr20">
+                <select
+                  className="w100p h50 p10 fz18 b-s1-grey"
+                  onChange={this.handleFormatChange}
+                >
+                  <option key="1" value="jpg">
+                    JPG
+                  </option>
+                  <option key="2" value="png">
+                    PNG
+                  </option>
+                  <option key="3" value="pdf">
+                    PDF
+                  </option>
+                </select>
+              </div>
+              <div className="w60p">
+                <button
+                  className="w100p h50 p10 fz18 bg-default cw fwb"
+                  onClick={() => {
+                    if (this.state.format === "pdf") {
+                      const dataURL = document
+                        .getElementsByClassName("canvasjs-chart-canvas")[0]
+                        .toDataURL();
+                      var pdf = new jsPDF();
+                      pdf.addImage(dataURL, "JPEG", 20, 20, 170, 80);
+                      pdf.save("download.pdf");
+                    } else {
+                      this.chart.exportChart({ format: this.state.format });
+                    }
+                  }}
+                >
+                  Atsisiūsti
+                </button>
+              </div>
             </div>
           </div>
         </div>
