68366d9b495144374bc45ffbeda693128655d412
Mantas Kemėšius <mantaskemesius@Mantass-MacBook-Air.local>
2019-05-30 23:28:58 +0300

    add trips filter

--- src/components/trips/table/rows/Container.js
+++ src/components/trips/table/rows/Container.js
@@ -3,10 +3,36 @@ import Rows from "./Component";
 
 const getFilteredIds = state => {
   const isAll = state.filters.activeTripFilter === "ALL";
-  if (isAll) return state.trips.ids;
-  return state.trips.ids.filter(
+  if (
+    !state.trips.ids.length &&
+    !state.user &&
+    !state.user.userRoleEnumList.length
+  )
+    return [];
+  const ida = state.trips.ids.filter(id => {
+    return (
+      (state.user &&
+        state.user.userRoleEnumList &&
+        !!state.user.userRoleEnumList.length &&
+        state.user.userRoleEnumList.indexOf("ADMINISTRATOR") !== -1) ||
+      checkOrAuthUserExistInTrip(state, id) ||
+      state.trips.byId[id].coordinator === state.user.uuid
+    );
+  });
+  if (isAll) return ida;
+  return ida.filter(
     id => state.trips.byId[id].status === state.filters.activeTripFilter
   );
+};
+
+const checkOrAuthUserExistInTrip = (state, tripId) => {
+  const ida = Object.keys(state.tripDetails.relatedTripIds).filter(
+    id => state.tripDetails.relatedTripIds[id] === tripId
+  );
+  const ids = ida.filter(
+    id => state.tripDetails.relatedUserIds[id] === state.user.uuid
+  );
+  return !!ids.length;
 };
 
 const mapStateToProps = state => {
