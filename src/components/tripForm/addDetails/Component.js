11edb30e48bdfe64e8379ae6a262a4d1f841bfc1
Mantas Kemėšius <mantaskemesius@Mantass-MacBook-Air.local>
2019-06-03 01:35:33 +0300

    fix problem

--- src/components/tripForm/addDetails/Component.js
+++ src/components/tripForm/addDetails/Component.js
@@ -22,7 +22,14 @@ class TripForm extends React.Component {
     employeeIds: [""],
     hotelNeeded: false,
     apartament: true,
-    userSaved: false
+    userSaved: false,
+    loader: false,
+    loader1: false,
+    loader2: false,
+    loader3: false,
+    loader4: false,
+    loader5: false,
+    loader6: false
   };
 
   getEmployeeSelectInputs = () => {
@@ -63,6 +70,15 @@ class TripForm extends React.Component {
     });
   };
 
+  setSleep = () => {
+    setTimeout(() => this.setState({ loader1: true }), 1000);
+    setTimeout(() => this.setState({ loader2: true }), 2000);
+    setTimeout(() => this.setState({ loader3: true }), 3000);
+    setTimeout(() => this.setState({ loader4: true }), 4000);
+    setTimeout(() => this.setState({ loader5: true }), 5000);
+    setTimeout(() => this.setState({ loader6: true }), 6000);
+  };
+
   addAdditionalField = () => {
     let { employeeIds } = this.state;
     employeeIds.push("");
@@ -99,10 +115,21 @@ class TripForm extends React.Component {
     });
     authPost(PATHS.TRIP_DETAILS_CREATE, postData).then(res => {
       authGet(PATHS.TRIP_DETAILS + "/" + this.props.tripId).then(res2 => {
+        this.setState({
+          loader: true
+        });
+        this.setSleep();
         authPut("/api/trip/setApts", res2.data).then(res3 => {
           this.setState({
             userSaved: true,
-            users: res3.data
+            users: res3.data,
+            loader: false,
+            loader1: false,
+            loader2: false,
+            loader3: false,
+            loader4: false,
+            loader5: false,
+            loader6: false
           });
         });
       });
@@ -121,13 +148,18 @@ class TripForm extends React.Component {
             {this.state.userSaved ? (
               <ul>
                 {this.state.users.map((user, key) => (
-                  <li key={key}>
+                  <li key={user.uuid}>
                     {user.appUser.name} {user.appUser.lastName} -
                     apgyvendinimas:{" "}
                     {user.hotelNeeded
                       ? "Viešbutyje"
-                      : "Apartamentuose, " +
-                        user.officeApartament.streetAddress}
+                      : "Apartamentuose, " + !!user.officeApartment &&
+                        !!user.officeApartment.streetAddress &&
+                        user.officeApartment.streetAddress +
+                          " nr. " +
+                          !!user.officeApartment &&
+                        !!user.officeApartment.apartmentNumber &&
+                        user.officeApartment.apartmentNumber}
                   </li>
                 ))}
               </ul>
@@ -147,14 +179,55 @@ class TripForm extends React.Component {
                         Prideti darbuotoja
                       </button>
                     </div>
-                    <div className="w100p pt20">
-                      <button
-                        className="w100p h50 p10 fz18 bg-success cw fwb"
-                        onClick={this.saveUsers}
-                      >
-                        Išsaugoti sąraša
-                      </button>
-                    </div>
+                    {!this.state.loader ? (
+                      <div className="w100p pt20">
+                        <button
+                          className="w100p h50 p10 fz18 bg-success cw fwb"
+                          onClick={this.saveUsers}
+                        >
+                          Išsaugoti sąraša
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="tac" style={{ marginTop: "10px" }}>
+                        <h1>Loading...</h1>
+                        <div
+                          className="df"
+                          style={{ justifyContent: "space-evenly" }}
+                        >
+                          {this.state.loader1 && (
+                            <div>
+                              <i className="fa fa-spinner fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                          {this.state.loader2 && (
+                            <div>
+                              <i className="fa fa-cog fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                          {this.state.loader3 && (
+                            <div>
+                              <i className="fa fa-spinner fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                          {this.state.loader4 && (
+                            <div>
+                              <i className="fa fa-cog fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                          {this.state.loader5 && (
+                            <div>
+                              <i className="fa fa-spinner fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                          {this.state.loader6 && (
+                            <div>
+                              <i className="fa fa-cog fa-spin fa-3x fa-fw" />
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    )}
                   </>
                 )}
               </>
